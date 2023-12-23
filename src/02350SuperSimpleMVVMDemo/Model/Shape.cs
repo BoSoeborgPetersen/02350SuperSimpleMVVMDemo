c214ffd368f8086f46b96adf585f727df418af4d
Bo Søborg Petersen <bo.petersen@outlook.dk>
2023-12-23 08:57:42 +0100

    Upgrade to .NET 8
    Reduce number of canvases

--- src/02350SuperSimpleMVVMDemo/Model/Shape.cs
+++ src/02350SuperSimpleMVVMDemo/Model/Shape.cs
@@ -2,7 +2,6 @@
 
 public record Shape(int X, int Y, int Width, int Height)
 {
-    public int CenterX => X + Width / 2;
-
-    public int CenterY => Y + Height / 2;
+    public int CenterX => X + (Width / 2);
+    public int CenterY => Y + (Height / 2);
 }
